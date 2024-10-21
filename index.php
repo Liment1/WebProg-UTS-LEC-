f4c44d29ded8b518a7bf929432b3be028567a802
Liment <69704267+Liment1@users.noreply.github.com>
2024-10-21 12:14:27 +0700

    update

--- index.php
+++ index.php
@@ -94,6 +94,7 @@
 </head>
 <body>
     <?php
+    require src\connection.php;
     session_start();
     if (!(isset($_SESSION['role'])) || $_SESSION['role'] != 'user') {
         header("Location: login.php");  
---
+++ src/connection.php
@@ -0,0 +1,3 @@
+<?php
+$connection = new PDO("mysql:host=localhost;dbname=umn_genap2122_pemweb_w6;", "root", "");
+?>
